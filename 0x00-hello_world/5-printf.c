5cab64569fe8a1a8a1bac55bb16cc7a79799a463
ElizabethTech <elizabethnwifir@gmail.com>
2023-06-19 16:21:06 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("With proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
